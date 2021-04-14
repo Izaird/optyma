152cdbe76657932c5c08bc477864cf750581d29d
Izaird Mothelet <iza_alex3@hotmail.com>
2021-04-14 18:47:50 -0500

    Creacion de nuevo documento para usar bloc

--- optyma/android/app/src/main/kotlin/com/example/optyma/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.optyma
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ optyma_app/android/app/src/main/kotlin/com/example/optyma_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.optyma_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
