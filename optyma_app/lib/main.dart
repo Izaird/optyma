16b82306ca147d0817d5ac7cd83d74230e2f999c
Izaird Mothelet <iza_alex3@hotmail.com>
2021-04-16 12:40:45 -0500

    Se integraron varias librerias al proyecto

--- optyma_app/lib/main.dart
+++ optyma_app/lib/main.dart
@@ -1,22 +1,56 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
- 
-void main() => runApp(MyApp());
- 
-class MyApp extends StatelessWidget {
+
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:optyma_app/bloc/my_bloc_observer.dart';
+import 'package:optyma_app/pages/home_page.dart';
+import 'package:optyma_app/pages/login/login_page.dart';
+import 'package:optyma_app/pages/splash_page.dart';
+import 'package:optyma_app/repository/authentication_repository.dart';
+import 'package:optyma_app/routes/app_router.dart';
+
+import 'bloc/authentication/authentication_bloc.dart';
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  Bloc.observer = MyBlocObserver();
+  final AuthenticationRepository authenticationRepository = AuthenticationRepository();
+
+  runApp(
+    RepositoryProvider.value(
+      value: authenticationRepository,
+      child: BlocProvider(
+        create: (context) => AuthenticationBloc(authenticationRepository: authenticationRepository)
+          ..add(AppStarted()),
+        child: App(),
+      ),
+    )
+  );
+}
+
+class App extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+      debugShowCheckedModeBanner: false,
+      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+        builder: (context, state) {
+          if (state is Uninitialized) {
+            return SplashPage();
+          }
+          if (state is Success) {
+            return HomePage();
+          }
+          if (state is Failure) {
+            return LoginPage();
+          }
+          return Container();
+        },
+      
       ),
+      // routes: getApplicationRoutes(),
     );
   }
 }
