1a9e155dbf4075eab0e1dedfe0891606092d0a6f
Izaird Mothelet <iza_alex3@hotmail.com>
2021-04-21 23:15:48 -0500

    Se agrego UserBloc de manera global

--- optyma_app/lib/main.dart
+++ optyma_app/lib/main.dart
@@ -4,11 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:bloc/bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:optyma_app/bloc/my_bloc_observer.dart';
-import 'package:optyma_app/pages/home_page.dart';
-import 'package:optyma_app/pages/login/login_page.dart';
-import 'package:optyma_app/pages/splash_page.dart';
+import 'package:optyma_app/bloc/user/user_bloc.dart';
+import 'package:optyma_app/pages/common/home/home_page.dart';
+import 'package:optyma_app/pages/common/login/login_page.dart';
+import 'package:optyma_app/pages/common/splash/splash_page.dart';
 import 'package:optyma_app/repository/authentication_repository.dart';
-import 'package:optyma_app/routes/app_router.dart';
+import 'package:optyma_app/repository/cloud_firestore_repository.dart';
+import 'package:optyma_app/routes/routes.dart';
 
 import 'bloc/authentication/authentication_bloc.dart';
 void main() async{
@@ -16,13 +18,28 @@ void main() async{
   await Firebase.initializeApp();
   Bloc.observer = MyBlocObserver();
   final AuthenticationRepository authenticationRepository = AuthenticationRepository();
+  final CloudFirestoreRepository cloudFirestoreRepository = CloudFirestoreRepository();
 
   runApp(
-    RepositoryProvider.value(
-      value: authenticationRepository,
-      child: BlocProvider(
-        create: (context) => AuthenticationBloc(authenticationRepository: authenticationRepository)
-          ..add(AppStarted()),
+    MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider.value(value: authenticationRepository),
+        RepositoryProvider.value(value: cloudFirestoreRepository),
+      ],
+
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => AuthenticationBloc(authenticationRepository: authenticationRepository)
+              ..add(AppStarted()),
+          ),
+          BlocProvider(
+            create: (context) => UserBloc(
+              authenticationRepository: authenticationRepository,
+              cloudFirestoreRepository: cloudFirestoreRepository
+            )
+          ),
+        ],
         child: App(),
       ),
     )
@@ -35,15 +52,17 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      routes: getApplicationRoutes(),
       home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
         builder: (context, state) {
-          if (state is Uninitialized) {
+          if (state is AuthenticationUninitialized) {
             return SplashPage();
           }
-          if (state is Success) {
+          if (state is AuthenticationSuccess) {
+            BlocProvider.of<UserBloc>(context).add(UserLoggedIn());
             return HomePage();
           }
-          if (state is Failure) {
+          if (state is AuthenticationFailure) {
             return LoginPage();
           }
           return Container();
