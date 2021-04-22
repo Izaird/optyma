11636762c80c9ba6b01e5f5f95d814f4eaed0805
Izaird Mothelet <iza_alex3@hotmail.com>
2021-04-21 23:05:14 -0500

    Creacion de la "homepage" del usuario

---
+++ optyma_app/lib/pages/user/home/home_user_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:optyma_app/bloc/authentication/authentication_bloc.dart';
+import 'package:optyma_app/bloc/user/user_bloc.dart';
+
+
+class HomeUserPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.exit_to_app),
+            onPressed: () {
+              BlocProvider.of<AuthenticationBloc>(context).add(LoggedOut());
+              BlocProvider.of<UserBloc>(context).add(UserLoggedOut());
+            },
+          )
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Center(child: Text('Bienvenido usuario!'),)
+        ],
+      ),
+    );
+  }
+}
