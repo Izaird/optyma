be116b34965a2f4715d6127601d2ad83fd87ea9e
Izaird Mothelet <iza_alex3@hotmail.com>
2021-04-25 17:20:49 -0500

    Se agregaron nuevas rutas

--- optyma_app/lib/routes/routes.dart
+++ optyma_app/lib/routes/routes.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:optyma_app/pages/admin/admins/add_admin_page.dart';
+import 'package:optyma_app/pages/admin/admins/admins_page.dart';
+import 'package:optyma_app/pages/admin/home/home_admin_paga.dart';
+import 'package:optyma_app/pages/admin/logro/logros_page.dart';
+import 'package:optyma_app/pages/admin/plantilla/add_plantilla_page.dart';
+import 'package:optyma_app/pages/admin/plantilla/plantillas_page.dart';
+import 'package:optyma_app/pages/admin/settings/settings_page.dart';
 
 
 import 'package:optyma_app/pages/common/home/home_page.dart';
 import 'package:optyma_app/pages/common/login/login_page.dart';
-import 'package:optyma_app/pages/admin/logro/logro_page.dart';
+import 'package:optyma_app/pages/admin/logro/add_logro_page.dart';
+import 'package:optyma_app/pages/common/signup/signup_page.dart';
 import 'package:optyma_app/pages/common/splash/splash_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes(){
   return <String, WidgetBuilder>{
-    'home'         : (_) => HomePage(),
+    //Common pages
+    'home'          : (_) => HomePage(),
     'splash'        : (_) => SplashPage(),
     'login'         : (_) => LoginPage(),
-    'logro'         : (_) => LogroPage(),
+    'signup'        : (_) => SignUpPage(),
+
+
+    //Admin Pages
+    'homeAdmin'     : (_) => HomeAdminPage(),
+    'logros'        : (_) => LogrosPage(),
+    'addLogro'      : (_) => AddLogroPage(),
+    'plantillas'    : (_) => PlantillasPage(),
+    'addPlantilla'  : (_) => AddPlantillaPage(),
+    'admins'        : (_) => AdminsPage(),
+    'addAdmin'      : (_) => AddAdminPage(),
+    'settings'      : (_) => SettingsPage(),
   };
 }
