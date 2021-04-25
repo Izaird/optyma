05768ce59aefd3aaddf6cde146ce6facaea511f5
Izaird Mothelet <iza_alex3@hotmail.com>
2021-04-25 17:26:10 -0500

    se agrego otro validator

--- optyma_app/lib/utils/validators.dart
+++ optyma_app/lib/utils/validators.dart
@@ -1,8 +1,29 @@
-final RegExp _expresionRegExp = RegExp(
-  r"[a-zA-Z ]{10,}");
+final RegExp _textRegExp = RegExp(
+  //TODO: Change the regexp so that only one name is required and  the second one is optional
+  r"^[A-Z][a-z]+\s[A-Z][a-z]+$");
+
+final RegExp _emailRegExp = RegExp(
+  r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+
+final RegExp _passwordRegExp = RegExp(
+  r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$");
 
 bool isText(String value){ 
   if(value.isEmpty) return false;
 
-  return _expresionRegExp.hasMatch(value);
+  return _textRegExp.hasMatch(value);
+}
+
+
+bool isEmail(String value){
+  if(value.isEmpty) return false;
+
+  return _emailRegExp.hasMatch(value);
+}
+
+
+bool isPassword(String value){
+  if(value.isEmpty) return false;
+
+  return _passwordRegExp.hasMatch(value);
 }
