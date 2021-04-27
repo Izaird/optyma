7cd8bdbbc139e775f842f56e173de7beca76aa88
Izaird Mothelet <iza_alex3@hotmail.com>
2021-04-26 21:19:48 -0500

    Se modifico la expresion regular para aceptar nombres

--- optyma_app/lib/utils/validators.dart
+++ optyma_app/lib/utils/validators.dart
@@ -1,6 +1,9 @@
+final RegExp _nameRegExp = RegExp(
+  r"[A-Za-z]+([\ A-Za-z]+)*");
+
 final RegExp _textRegExp = RegExp(
-  //TODO: Change the regexp so that only one name is required and  the second one is optional
-  r"^[A-Z][a-z]+\s[A-Z][a-z]+$");
+  r"^[a-zA-Z ]*$"
+);
 
 final RegExp _emailRegExp = RegExp(
   r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
@@ -8,22 +11,22 @@ final RegExp _emailRegExp = RegExp(
 final RegExp _passwordRegExp = RegExp(
   r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$");
 
-bool isText(String value){ 
+bool isName(String value){ 
   if(value.isEmpty) return false;
-
-  return _textRegExp.hasMatch(value);
+  return _nameRegExp.hasMatch(value);
 }
-
 
 bool isEmail(String value){
   if(value.isEmpty) return false;
-
   return _emailRegExp.hasMatch(value);
 }
 
-
 bool isPassword(String value){
   if(value.isEmpty) return false;
-
   return _passwordRegExp.hasMatch(value);
+}
+
+bool isText(String value){
+  if(value.isEmpty) return false;
+  return _textRegExp.hasMatch(value);
 }
