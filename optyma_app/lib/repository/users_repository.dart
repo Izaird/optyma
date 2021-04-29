dd797fe13a4b5fbe08ceba0c47ae452dd5869b8a
Izaird Mothelet <iza_alex3@hotmail.com>
2021-04-29 05:07:48 -0500

    Se modifico la el model UserModel

--- optyma_app/lib/repository/users_repository.dart
+++ optyma_app/lib/repository/users_repository.dart
@@ -1,22 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:optyma_app/models/user.dart';
+import 'package:optyma_app/models/user_model.dart';
 
 
 class UsersRepository{
 
   CollectionReference usersReference = FirebaseFirestore.instance.collection('users');
 
-  Future<void> updateUserData(User user) async { 
-    DocumentReference refUser = usersReference.doc(user.uid);
-
-    return await refUser.set({
-      'admin'       : user.admin,
-      'uId'         : user.uid,
-      'name'        : user.name,
-      'email'       : user.email,
-      'lastSignIn'  : DateTime.now(),
-    });
-  }
 
   Future<String> getUserName(String userId) async{  
     DocumentReference refUser = usersReference.doc(userId);
@@ -30,16 +19,6 @@ class UsersRepository{
     } 
   }
 
-
-  Future<void> updateUserName(String name, String userId) async{  
-    DocumentReference refUser = usersReference.doc(userId);
-
-    return await refUser.set({
-      'name'        : name
-    }); 
-  }
-
-
   Future<bool> isAdmin(userId) async{
     DocumentReference refUser = usersReference.doc(userId);
     DocumentSnapshot doc = await refUser.get();
@@ -51,5 +30,57 @@ class UsersRepository{
       return false;
     }
   }
+
+  Stream<List<UserModel>> getAdmins(String userId) {
+    return usersReference
+    .where('admin', isEqualTo: true)
+    .where('uId', isNotEqualTo: userId)
+    .snapshots().map((snapshot){
+      return snapshot.docs.map((doc) => UserModel.fromSnapshot(doc)).toList();
+    });
+
+  }
+
+
+  Stream<List<UserModel>> getNotAdmins(String userId) {
+    return usersReference
+    .where('admin', isEqualTo: false)
+    .where('uId', isNotEqualTo: userId)
+    .snapshots().map((snapshot){
+      return snapshot.docs.map((doc) => UserModel.fromSnapshot(doc)).toList();
+    });
+
+  }
+
+
+  Future<void> updateUserData(UserModel user) async { 
+    DocumentReference refUser = usersReference.doc(user.uid);
+
+    return await refUser.set({
+      'admin'       : user.admin,
+      'uId'         : user.uid,
+      'name'        : user.name,
+      'email'       : user.email,
+      'lastSignIn'  : DateTime.now(),
+    });
+  }
+
+  Future<void> updateUserAdminStatus({String userId, bool admin}) async{
+    DocumentReference refUser = usersReference.doc(userId);
+
+    return await refUser.update({
+      'admin'       : admin 
+    });
+  }
+
+  Future<void> updateUserName(String name, String userId) async{  
+    DocumentReference refUser = usersReference.doc(userId);
+
+    return await refUser.set({
+      'name'        : name
+    }); 
+  }
+
+
 
 }
