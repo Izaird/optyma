2a9b4c5befa36c0a04417a73132d0c73fcaacce1
Izaird Mothelet <iza_alex3@hotmail.com>
2021-04-28 23:28:38 -0500

    Se agrego el constructor fromSnapshot a LogroModel

--- optyma_app/lib/models/logro_model.dart
+++ optyma_app/lib/models/logro_model.dart
@@ -4,6 +4,8 @@
 
 import 'dart:convert';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 LogroModel logrosModelFromJson(String str) => LogroModel.fromJson(json.decode(str));
 
 String logrosModelToJson(LogroModel data) => json.encode(data.toJson());
@@ -24,6 +26,14 @@ class LogroModel {
         name: json["name"],
         description: json["description"],
     );
+
+    factory LogroModel.fromSnapshot(DocumentSnapshot snapshot){
+      return LogroModel(
+        id          : snapshot.id,
+        name        : snapshot.data()['nombre'],
+        description : snapshot.data()['descripcion'],
+      );
+    }
 
     Map<String, dynamic> toJson() => {
         "id": id,
