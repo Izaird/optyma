417e3a934033f8d82cb4eaa17faad56a9f71d767
Izaird Mothelet <iza_alex3@hotmail.com>
2021-05-27 18:26:37 -0500

    Creacion del Widget LogrosListBlocBuilder para mostrar los logros

--- optyma_app/lib/pages/admin/logro/logros_list/logros_list_page.dart
+++ optyma_app/lib/pages/admin/logro/logros_list/logros_list_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:optyma_app/bloc/logros_list/logros_list_bloc.dart';
+import 'package:optyma_app/models/logro_model.dart';
 import 'package:optyma_app/repository/logros_repository.dart';
 
 
@@ -36,7 +37,12 @@ class LogrosListBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Column(
+      children: [
+        MenuTypesOfLogros(),
+        LogrosListBlocBuilder(),
+      ],
+    );
   }
 }
 
@@ -59,6 +65,7 @@ class _MenuTypesOfLogrosState extends State<MenuTypesOfLogros> {
           child: Text('Niveles'),
           onTap: () {
             BlocProvider.of<LogrosListBloc>(context).add(LogrosListType1Selected());
+            BlocProvider.of<LogrosListBloc>(context).add(LogrosListLoaded());
           },
         ),
         DropdownMenuItem(
@@ -66,6 +73,7 @@ class _MenuTypesOfLogrosState extends State<MenuTypesOfLogros> {
           child: Text('Racha de respuestas'),
           onTap: () {
             BlocProvider.of<LogrosListBloc>(context).add(LogrosListType2Selected());
+            BlocProvider.of<LogrosListBloc>(context).add(LogrosListLoaded());
           },
         ),
         DropdownMenuItem(
@@ -73,6 +81,7 @@ class _MenuTypesOfLogrosState extends State<MenuTypesOfLogros> {
           child: Text('Racha de días'),
           onTap: () {
             BlocProvider.of<LogrosListBloc>(context).add(LogrosListType3Selected());
+            BlocProvider.of<LogrosListBloc>(context).add(LogrosListLoaded());
           },
         ),
         DropdownMenuItem(
@@ -80,6 +89,7 @@ class _MenuTypesOfLogrosState extends State<MenuTypesOfLogros> {
           child: Text('Ejercicios realizados'),
           onTap: () {
             BlocProvider.of<LogrosListBloc>(context).add(LogrosListType4Selected());
+            BlocProvider.of<LogrosListBloc>(context).add(LogrosListLoaded());
           },
         ),
         DropdownMenuItem(
@@ -87,6 +97,7 @@ class _MenuTypesOfLogrosState extends State<MenuTypesOfLogros> {
           child: Text('Leaderboard'),
           onTap: () {
             BlocProvider.of<LogrosListBloc>(context).add(LogrosListType5Selected());
+            BlocProvider.of<LogrosListBloc>(context).add(LogrosListLoaded());
           },
         ),
       ],
@@ -100,19 +111,76 @@ class _MenuTypesOfLogrosState extends State<MenuTypesOfLogros> {
 }
 
 
+//
+class LogrosListBlocBuilder extends StatelessWidget {
 
-class LogrosList extends StatelessWidget {
-
+  List<LogroModel> logrosList = [];
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<LogrosListBloc, LogrosListState>(
       builder: (context, state) {
-        if(state is LogrosListInitial){
-          return Text('Escoge un tipo de logro');
+        if(state is LogrosListLoadInProgress){
+          return Center(child: CircularProgressIndicator());
         }
-        if(state is LogrosListBody)
+        if(state is LogrosListLoadFailure){
+          return Center(
+            child: Column(  
+              children: [
+                Icon(Icons.error),
+                Text('Hubo un error al cargar la información'),
+              ],
+            )
+          );
+        }
+        if(state is LogrosListLoadSuccess){
+          logrosList = state.logros;
+          return Container(
+            child: logrosList.length == 0 
+            ?Center(child: Text('Logros no disponibles'))
+            :Expanded(
+              child: ListView.builder(
+                itemCount: logrosList.length,
+                itemBuilder: (context, i){
+                  return CreateItem(context: context, logro: logrosList[i]);
+                },
+              ),
+            ),
+          );
+        }
         return Container();
       },
+    );
+  }
+}
+
+class CreateItem extends StatelessWidget {
+
+  final BuildContext context;
+  final LogroModel logro;
+
+  const CreateItem({
+    Key key, 
+    this.context, 
+    this.logro
+    }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Dismissible(
+      key: UniqueKey(),
+      background: Container( 
+        color: Colors.red,
+      ),
+      onDismissed: (direction) {
+        RepositoryProvider.of<LogrosRepository>(context).deleteLogro(logro.id);
+      },
+      child: ListTile(   
+        title: Text('${logro.name}'),
+        subtitle: Text('${logro.description}'),
+        onTap: () {
+          //TODO: add functionality to edit logro
+        },
+      ),
     );
   }
 }
