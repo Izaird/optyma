8be696c26cf5dc82b56dc4a7de2419bb85930ba4
Izaird Mothelet <iza_alex3@hotmail.com>
2021-05-27 04:54:32 -0500

    se agregaron funciones para agregar diferentes tipos de logros

--- optyma_app/lib/repository/logros_repository.dart
+++ optyma_app/lib/repository/logros_repository.dart
@@ -1,30 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:optyma_app/models/logro_model.dart';
-import 'package:optyma_app/models/logros/logro_type1_model.dart';
-import 'package:optyma_app/models/logros/logro_type2_model.dart';
-import 'package:optyma_app/models/logros/logro_type3_model.dart';
-import 'package:optyma_app/models/logros/logro_type4_model.dart';
-import 'package:optyma_app/models/logros/logro_type5_model.dart';
 
 class LogrosRepository{
 
   CollectionReference logrosReference = FirebaseFirestore.instance.collection('logros');
 
-
-  Future<void> addLogro( LogroModel logro ) async{
+  //Niveles
+  Future<void> addLogroType1( LogroModel logro ) async{
     logrosReference.add({
-      'nombre'      : logro.name,
-      'descripcion'  : logro.description 
-    })
-      .then((value) => print(value))
-      .catchError((error)=> print("Failed to add logro: $error"));
-  }
-
-  Future<void> addLogroType1( LogroType1Model logro ) async{
-    logrosReference.add({
-      'nombre'      : logro.name,
-      'descripcion' : logro.description,
+      'type'        : logro.type,
+      'name'        : logro.name,
+      'description' : logro.description,
       'difficulty'  : logro.difficulty,
       'subject'     : logro.subject
     })
@@ -32,39 +19,48 @@ class LogrosRepository{
       .catchError((error)=> print("Failed to add logro: $error"));
   }
 
-Future<void> addLogroType2( LogroType2Model logro ) async{
+  //
+  Future<void> addLogroType2( LogroModel logro ) async{
     logrosReference.add({
-      'nombre'      : logro.name,
-      'descripcion' : logro.description,
-      'data'        : logro.data
+      'type'            : logro.type,
+      'name'            : logro.name,
+      'description'     : logro.description,
+      'numberOfAnswers' : logro.numberOfAnswers,
     })
       .then((value) => print(value))
       .catchError((error)=> print("Failed to add logro: $error"));
   }
-  Future<void> addLogroType3( LogroType3Model logro ) async{
+  
+  //Racha de dias 
+  Future<void> addLogroType3( LogroModel logro ) async{
     logrosReference.add({
-      'nombre'      : logro.name,
-      'descripcion' : logro.description,
-      'data'        : logro.data
-    })
-      .then((value) => print(value))
-      .catchError((error)=> print("Failed to add logro: $error"));
-  }
-Future<void> addLogroType4( LogroType4Model logro ) async{
-    logrosReference.add({
-      'nombre'      : logro.name,
-      'descripcion' : logro.description,
-      'data'        : logro.data
+      'type'            : logro.type,
+      'name'            : logro.name,
+      'description'     : logro.description,
+      'numberOfDays'    : logro.numberOfDays,
     })
       .then((value) => print(value))
       .catchError((error)=> print("Failed to add logro: $error"));
   }
 
-Future<void> addLogroType5( LogroType5Model logro ) async{
+  //Ejercicios realizados
+  Future<void> addLogroType4( LogroModel logro ) async{
     logrosReference.add({
+      'type'              : logro.type,
+      'name'              : logro.name,
+      'description'       : logro.description,
+      'numberOfExercises' : logro.numberOfExercises,
+    })
+      .then((value) => print(value))
+      .catchError((error)=> print("Failed to add logro: $error"));
+  }
+
+  //Leaderboard
+  Future<void> addLogroType5( LogroModel logro ) async{
+    logrosReference.add({
+      'type'        : logro.type,
       'nombre'      : logro.name,
       'descripcion' : logro.description,
-      'difficulty'  : logro.difficulty,
       'subject'     : logro.subject
     })
       .then((value) => print(value))
