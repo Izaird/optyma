2297fff363007b8ee3aaa01da2824321223a7e13
Izaird Mothelet <iza_alex3@hotmail.com>
2021-08-01 21:11:21 -0500

    Protocolo para TTR

---
+++ optyma_app/lib/utils/theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+enum AppTheme{
+  BlueLight,
+  BlueDark,
+}
+
+final appThemeData = {
+  AppTheme.BlueLight: ThemeData(
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+  ),
+  AppTheme.BlueDark: ThemeData(
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+  ),
+};
