8aaec6b9fb3593c76a5225d59ec4995dde0b405c
Izaird Alexander Mothelet Delgado <31055847+Izaird@users.noreply.github.com>
2021-09-04 20:02:28 -0500

    Se agregaron ValueFailures y ValueValidators

--- optyma_app/lib/domain/core/value_failure.dart
+++ optyma_app/lib/domain/core/value_failure.dart
@@ -7,7 +7,21 @@ class ValueFailure<T> with _$ValueFailure<T> {
   const factory ValueFailure.invalidEmail({
     required T failedValue,
   }) = InvalidEmail<T>;
+  
   const factory ValueFailure.invalidPassword({
     required T failedValue,
   }) = ShortPassword<T>;
+
+  const factory ValueFailure.exceedingLength({
+    required T failedValue,
+    required int max
+  }) = ExceedingLength<T>;
+
+  const factory ValueFailure.empty({
+    required T failedValue,
+  }) = Empty<T>;
+
+  const factory ValueFailure.multiline({
+    required T failedValue,
+  }) = Multiline<T>;
 }
--- optyma_app/lib/domain/core/value_validators.dart
+++ optyma_app/lib/domain/core/value_validators.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:kt_dart/collection.dart';
 import 'value_failure.dart';
 
 Either<ValueFailure<String>, String> validateEmailAddress(String input) {
@@ -21,3 +22,37 @@ Either<ValueFailure<String>, String> validatePassword(String input) {
     return left(ValueFailure.invalidPassword(failedValue: input));
   }
 }
+
+Either<ValueFailure<String>, String> validateMaxStringLength(
+  String input,
+  int maxLength
+) {
+  if(input.length <= maxLength){
+    return right(input);
+  }else{
+    return left(
+      ValueFailure.exceedingLength(failedValue: input, max: maxLength),
+    );
+  }
+}
+
+Either<ValueFailure<String>, String> validateStringNotEmpty(String input) {
+  if(input.isNotEmpty){
+    return right(input);
+  }else{
+    return left(
+      ValueFailure.empty(failedValue: input),
+    );
+  }
+}
+
+Either<ValueFailure<String>, String> validateSingleLine(String input) {
+  if(!input.contains('\n')){
+    return right(input);
+  }else{
+    return left(
+      ValueFailure.multiline(failedValue: input)
+    );
+  }
+}
+
