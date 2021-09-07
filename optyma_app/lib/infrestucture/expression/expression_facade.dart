908ecf7a866cec1ab5b8651475a9ebed88891138
ItzcoR <itzcoatlpv@gmail.com>
2021-09-07 13:18:05 -0500

    Implemented expression_facade.dart

--- optyma_app/lib/infrestucture/expression/expression_facade.dart
+++ optyma_app/lib/infrestucture/expression/expression_facade.dart
@@ -21,8 +21,95 @@ class ExpressionFacade implements IExpressionFacade{
   }
 
   @override
-  EvaluatedExpression questionInt(operationType operationType, difficulty difficulty) {
+  EvaluatedExpression questionInt(opt operationType, difficulty diff) {
     // TODO: implement questionInt
-    throw UnimplementedError();
+    Random rand= Random();
+    int numberOfVars=0;
+    List<int>vars = [];
+    String operation='';
+    List<int>listOfOptions=[];
+    int result=0;
+    int i=0;
+    if(operationType == opt.addition || operationType == opt.substraction){
+      numberOfVars = 2 + rand.nextInt(3);
+    }
+    else if(operationType==opt.multiplication){
+      numberOfVars = 2 + rand.nextInt(2);
+    }
+    else{
+      numberOfVars = 2;
+    }
+    if(diff == difficulty.easy){
+      for(i=0;i<numberOfVars;i++){
+        vars.add(rand.nextInt(25)+1);
+      }
+    }
+    else if(diff == difficulty.medium){
+      for(i=0;i<numberOfVars;i++){
+        vars.add(rand.nextInt(99)+10);
+      }
+    }
+    else if (diff == difficulty.hard){
+       for(i=0;i<numberOfVars;i++){
+        vars.add(rand.nextInt(979)+20);
+      }
+    }
+
+    if(operationType == opt.addition){
+      for(i=0;i<numberOfVars;i++){
+        result= result + vars[i];
+        if(i == numberOfVars - 1) {
+          operation = operation + vars[i].toString();
+        } else{
+          operation = '$operation${vars[i]}+';
+        }
+      }
+    }
+    else if(operationType == opt.substraction){
+      result=vars[0];
+      for(i=0;i<vars.length;i++){
+        if(i == vars.length - 1) {
+          operation = operation + vars[i].toString();
+        } else{
+          operation = '$operation${vars[i]}-';
+        }
+        for(i=1;i<vars.length;i++){
+          result=result-vars[i];
+        }
+      }
+    }
+    else if(operationType == opt.multiplication){
+      for(i=0;i<numberOfVars;i++){
+        if(i == numberOfVars - 1) {
+          operation = operation + vars[i].toString();
+        } else{
+          operation = '$operation${vars[i]}*';
+        }
+      }
+    }
+    else if(operationType == opt.division){
+      int factor1=0;
+      int factor2=0, num1=0;
+
+      if(diff==difficulty.easy){
+        factor1= rand.nextInt(12)+10;
+        factor2 = rand.nextInt(10)+1;
+      }
+      if(diff==difficulty.medium){
+        factor1= rand.nextInt(30)+10;
+        factor2 = rand.nextInt(16)+2;
+      }if(diff==difficulty.hard){
+        factor1= rand.nextInt(61)+10;
+        factor2 = rand.nextInt(24)+2;
+      }
+      num1 = factor1*factor2;
+      operation = '$num1/$factor2';
+    }
+    else{
+      print("Unexpected operation type");
+    }
+    listOfOptions = _expressionOptions(result);
+    return EvaluatedExpression(evalutedExpression: operation, result: result, options: listOfOptions);
+    //throw UnimplementedError();
   }
 } 
