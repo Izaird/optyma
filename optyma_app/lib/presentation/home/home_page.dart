8e9a1eb3cc2741569535cd39a8bca4b697982d7a
ItzcoR <itzcoatlpv@gmail.com>
2021-09-15 18:37:42 -0500

    Añadidos los botones de logros y leaderboards a la pantalla principal

--- optyma_app/lib/presentation/home/home_page.dart
+++ optyma_app/lib/presentation/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:optyma_app/presentation/core/player_navigation_menu.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -11,10 +12,34 @@ class HomePage extends StatelessWidget {
       drawer: PlayerNavigationMenu(),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children:const [
-          Center(
+        children: [
+          const Center(
             child: Text('Bienvenido usuario!')
-          )
+          ),
+          ListTile(
+            leading: const Icon(FontAwesomeIcons.gamepad, color: Colors.blue),
+            title: const Text('Juegos'),
+            onTap: (){
+              //Navigator.pop(context);
+              Navigator.pushNamed(context, 'game-modes');
+            },
+          ),
+          ListTile(
+            leading: const Icon(FontAwesomeIcons.trophy, color: Colors.blue),
+            title: const Text('Logros'),
+            onTap: (){
+              //Navigator.pop(context);
+              //Navigator.pushNamed(context, 'game-modes');
+            },
+          ),
+          ListTile(
+            leading: const Icon(FontAwesomeIcons.table, color: Colors.blue),
+            title: const Text('Leaderboards'),
+            onTap: (){
+              //Navigator.pop(context);
+              //Navigator.pushNamed(context, 'game-modes');
+            },
+          ),
         ],
       ),
     );
